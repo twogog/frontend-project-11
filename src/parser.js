a9b772db1047a57b2a608cd2e54232378c216161
twogog <twogogtwogog@gmail.com>
2023-01-24 06:10:55 +0600

    trying to fix no inet connection error

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,9 @@
 export default (url) => {
   const parse = new DOMParser();
-  return fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
-    .then((response) => {
-      if (response.ok) { return response.json(); }
-      throw new Error('Ошибка сети.');
-    })
+  const response = fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`);
+  return response.then((result) => {
+    if (result.ok) { return result.json(); }
+    throw new Error('Ошибка сети.');
+  })
     .then((data) => parse.parseFromString(`${data.contents}`, 'application/xml'));
 };
